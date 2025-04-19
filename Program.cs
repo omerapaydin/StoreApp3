883285f8f9111cd6cd9439d38b28102eb14122c7
Ömer Apaydın <omerapaydin90@gmail.com>
2025-04-20 00:29:58 +0300

    Program.cs temel

--- Program.cs
+++ Program.cs
@@ -1,6 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllersWithViews();
+
+
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+app.UseRouting();
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+
+app.MapControllerRoute(
+    name:"default",
+    pattern:"{controller=Home}/{action=Index}/{id?}"
+);
+
 
 app.Run();
